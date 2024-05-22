e092897caa689f66e16119ede7a3e870c0ec4930
aliceohly <alicewuliying@gmail.com>
2024-05-22 14:06:41 +1000

    fix: move authOptions back to the route.ts file, remove keyword export infront of authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,32 @@
-
-import { authOptions } from "@/lib/auth";
+import { mergeCarts } from "@/lib/db/cart";
+import { prisma } from "@/lib/db/prisma";
+import { env } from "@/lib/env";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeCarts(user.id);
+    },
+  },
+};
 
 const handler = NextAuth(authOptions);
 
