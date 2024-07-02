2e0da659507baa05c2bdec5b086773a56b007ec4
aliceohly <alicewuliying@gmail.com>
2024-07-02 11:38:12 +1000

    - add more products - add pagination bar

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "images.unsplash.com" },
+      { hostname: "plus.unsplash.com" },
+      { hostname: "www.thespruceeats.com" },
+      { hostname: "aubreyskitchen.com" },
+      { hostname: "www.tasteofhome.com" },
       { hostname: "lh3.googleusercontent.com" },
     ],
   },
